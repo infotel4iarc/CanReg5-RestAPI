a8ac2840316bb3a610738de456fe2fd3da757d98
wninfotel <1foCMAO!>
2022-01-27 10:38:17 +0100

    Implement GETs

--- src/main/java/fr/iarc/canreg/restapi/controller/DataController.java
+++ src/main/java/fr/iarc/canreg/restapi/controller/DataController.java
@@ -1,17 +1,20 @@
 package fr.iarc.canreg.restapi.controller;
 
 
+import canreg.common.database.DatabaseRecord;
 import canreg.common.database.Patient;
 import canreg.common.database.PopulationDataset;
 import canreg.common.database.Source;
 import canreg.common.database.Tumour;
 import canreg.server.database.RecordLockedException;
 import fr.iarc.canreg.restapi.service.DataService;
+import lombok.SneakyThrows;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.MediaType;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -24,35 +27,50 @@ public class DataController {
     @Autowired
     private DataService dataService;
 
+    /**
+     * @return Map
+     */
     @GetMapping(path = "/populations", produces = MediaType.APPLICATION_JSON_VALUE)
     public ResponseEntity<Map<Integer, PopulationDataset>> getPopulations() {
         Map<Integer, PopulationDataset> populations = dataService.getPopulations();
-
-        if (populations == null) {
-            return new ResponseEntity<>(HttpStatus.NOT_FOUND);
-        }
+        // populations is never null
         return new ResponseEntity<>(populations, HttpStatus.OK);
 
     }
 
-    @GetMapping(path = "/patients", produces = MediaType.APPLICATION_JSON_VALUE)
-    public ResponseEntity getPatient() throws RecordLockedException {
-        Patient patient = dataService.getPatient(1, false);
-        if (patient == null) {
+    /**
+     * @param recordID
+     * @return record content, null if not found and locked if there's an exception
+     */
+    @SneakyThrows
+    @GetMapping(path = "/patients/{recordID}", produces = MediaType.APPLICATION_JSON_VALUE)
+    public ResponseEntity<Patient> getPatient(@PathVariable("recordID") Integer recordID) {
+
+        try {
+            dataService.getPatient(recordID);
+        } catch (RecordLockedException e) {
+            return new ResponseEntity<>(HttpStatus.LOCKED);
+        }
+        if ((dataService.getPatient(recordID)) == null) {
             return new ResponseEntity(HttpStatus.NOT_FOUND);
         }
-        return new ResponseEntity(patient, HttpStatus.OK);
+        return new ResponseEntity<>(HttpStatus.OK);
 
     }
 
     @GetMapping(path = "/tumours", produces = MediaType.APPLICATION_JSON_VALUE)
-    public ResponseEntity getTumours(int recordID) throws RecordLockedException {
+    public ResponseEntity<Tumour> getTumours(@PathVariable("recordID") Integer recordID){
 
-        Tumour tumour = dataService.getTumours(recordID, false);
+        Tumour tumour = null;
+        try {
+            tumour = dataService.getTumours(recordID);
+        } catch (RecordLockedException e) {
+            e.printStackTrace();
+        }
         if (tumour != null) {
             return new ResponseEntity(tumour, HttpStatus.OK);
         }
-        return new ResponseEntity( HttpStatus.NOT_FOUND);
+        return new ResponseEntity(HttpStatus.NOT_FOUND);
     }
 
 
@@ -65,7 +83,6 @@ public class DataController {
 
         return new ResponseEntity(source, HttpStatus.OK);
     }
-
 
 
 }
--- src/main/java/fr/iarc/canreg/restapi/service/DataService.java
+++ src/main/java/fr/iarc/canreg/restapi/service/DataService.java
@@ -8,48 +8,72 @@ import canreg.common.database.Source;
 import canreg.common.database.Tumour;
 import canreg.server.database.CanRegDAO;
 import canreg.server.database.RecordLockedException;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
 
 import java.util.Map;
 
-public class  DataService {
+@Service
+public class DataService implements DataServiceInterface {
+    private static final Logger LOGGER = LoggerFactory.getLogger(DataService.class);
 
     @Autowired
     public CanRegDAO canRegDAO;
 
 
+    /**
+     * @return return Map, never null
+     */
     public Map<Integer, PopulationDataset> getPopulations() {
         return canRegDAO.getPopulationDatasets();
     }
 
-    public Patient getPatient(int recordID, boolean lock) throws RecordLockedException {
+    /**
+     * Returns null if the record cannot be read
+     * @param recordID id of a patient
+     * @return a patient
+     * @throws RecordLockedException
+     */
+    public Patient getPatient(Integer recordID) throws RecordLockedException {
+
+        DatabaseRecord p =  canRegDAO.getRecord(recordID, Globals.PATIENT_TABLE_NAME, false);
+
+
+        System.err.println( " patient "+ p);
         return (Patient) canRegDAO.getRecord(recordID, Globals.PATIENT_TABLE_NAME, false);
     }
 
-    public Tumour getTumours(int recordID, boolean lock) throws RecordLockedException {
-        return (Tumour) canRegDAO.getRecord(recordID, Globals.TUMOUR_TABLE_NAME, false);
-    }
 
-    public Source getSources(int recordID, boolean lock) throws RecordLockedException {
-        return (Source) canRegDAO.getRecord(recordID, Globals.SOURCE_TABLE_NAME, false);
-    }
-
-
-    public DatabaseRecord getRecord(int recordID, String tableName, boolean lock) throws RecordLockedException {
-
-        switch (tableName) {
-            case Globals.PATIENT_TABLE_NAME:
-                return getPatient(recordID, lock);
-
-            case Globals.TUMOUR_TABLE_NAME:
-                return getTumours(recordID, lock);
-
-            case Globals.SOURCE_TABLE_NAME:
-                return getSources(recordID, lock);
-
-            default:
-                return null;
+        /**
+         * @param recordID
+         * @return
+         * @throws RecordLockedException
+         */
+        public Tumour getTumours ( int recordID) throws RecordLockedException {
+            return (Tumour) canRegDAO.getRecord(recordID, Globals.TUMOUR_TABLE_NAME, false);
         }
-    }
 
-}
+        public Source getSources ( int recordID, boolean lock) throws RecordLockedException {
+            return (Source) canRegDAO.getRecord(recordID, Globals.SOURCE_TABLE_NAME, false);
+        }
+
+
+        public DatabaseRecord getRecord ( int recordID, String tableName,boolean lock) throws RecordLockedException {
+            switch (tableName) {
+                case Globals.PATIENT_TABLE_NAME:
+                    return getPatient(recordID);
+
+                case Globals.TUMOUR_TABLE_NAME:
+                    return getTumours(recordID);
+
+                case Globals.SOURCE_TABLE_NAME:
+                    return getSources(recordID, lock);
+
+                default:
+                    return null;
+            }
+        }
+
+    }
