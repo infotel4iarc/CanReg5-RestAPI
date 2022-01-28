49ceadc2d9eeb7a384135a4c52c11aec92c3c463
sminfotel <stephane.maheo@infotel.com>
2022-01-28 16:34:25 +0100

    Add swagger

---
+++ src/main/java/fr/iarc/canreg/restapi/configuration/Swagger2Configuration.java
@@ -0,0 +1,59 @@
+package fr.iarc.canreg.restapi.configuration;
+
+
+import com.fasterxml.jackson.databind.DeserializationFeature;
+import com.fasterxml.jackson.databind.ObjectMapper;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.http.converter.HttpMessageConverter;
+import org.springframework.http.converter.json.MappingJackson2HttpMessageConverter;
+import org.springframework.web.servlet.config.annotation.EnableWebMvc;
+import org.springframework.web.servlet.config.annotation.ResourceHandlerRegistry;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurerAdapter;
+import springfox.documentation.builders.ApiInfoBuilder;
+import springfox.documentation.builders.PathSelectors;
+import springfox.documentation.builders.RequestHandlerSelectors;
+import springfox.documentation.service.ApiInfo;
+import springfox.documentation.spi.DocumentationType;
+import springfox.documentation.spring.web.plugins.Docket;
+import springfox.documentation.swagger2.annotations.EnableSwagger2;
+
+import java.util.Arrays;
+import java.util.HashSet;
+import java.util.List;
+
+
+@EnableWebMvc
+@Configuration
+@EnableSwagger2
+public class Swagger2Configuration extends WebMvcConfigurerAdapter {
+    @Override
+    public void extendMessageConverters(List<HttpMessageConverter<?>> converters) {
+        converters.add(mappingJackson2HttpMessageConverter());
+    }
+    @Bean
+    public MappingJackson2HttpMessageConverter mappingJackson2HttpMessageConverter() {
+        MappingJackson2HttpMessageConverter converter = new MappingJackson2HttpMessageConverter();
+        converter.setObjectMapper(
+                new ObjectMapper().configure(DeserializationFeature.FAIL_ON_UNKNOWN_PROPERTIES, false));
+        return converter;
+    }
+    @Bean
+    public Docket docket() {
+        Docket docket = new Docket(DocumentationType.SWAGGER_2);
+        docket.protocols(new HashSet<String>(Arrays.asList("http")));
+        docket.apiInfo(apiInfo()).select()
+                .apis(RequestHandlerSelectors.basePackage("fr.iarc.canreg.restapi"))
+                .paths(PathSelectors.any()).build();
+        return docket;
+    }
+    @Override
+    public void addResourceHandlers(ResourceHandlerRegistry registry) {
+        registry.addResourceHandler("swagger-ui.html").addResourceLocations("classpath:/META-INF/resources/");
+        registry.addResourceHandler("/webjars/**").addResourceLocations("classpath:/META-INF/resources/webjars/");
+    }
+    private ApiInfo apiInfo() {
+        return new ApiInfoBuilder().title("CanReg RESTful API")
+                .description("Browse and interact with RESTful API dynamically").build();
+    }
+}
