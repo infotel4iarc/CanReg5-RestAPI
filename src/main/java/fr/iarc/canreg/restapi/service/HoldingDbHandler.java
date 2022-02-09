c868c805d065c5251c2e9b7ad70a87b7f596a913
sminfotel <stephane.maheo@infotel.com>
2022-02-09 16:20:35 +0100

    Handle the additional variables for holding db

--- src/main/java/fr/iarc/canreg/restapi/service/HoldingDbHandler.java
+++ src/main/java/fr/iarc/canreg/restapi/service/HoldingDbHandler.java
@@ -1,10 +1,9 @@
 package fr.iarc.canreg.restapi.service;
 
-import canreg.common.database.User;
+import canreg.common.database.HoldingDbCommon;
 import canreg.server.CanRegServerImpl;
 import canreg.server.database.CanRegDAO;
 import canreg.server.management.SystemDescription;
-import java.io.IOException;
 import java.util.Properties;
 import lombok.RequiredArgsConstructor;
 import org.slf4j.Logger;
@@ -15,21 +14,25 @@ import org.slf4j.LoggerFactory;
  */
 @RequiredArgsConstructor
 public class HoldingDbHandler {
-  private static final Logger LOGGER = LoggerFactory.getLogger(HoldingDbHandler.class);
+    private static final Logger LOGGER = LoggerFactory.getLogger(HoldingDbHandler.class);
 
-  public final CanRegDAO canRegDAO;
-  public final Properties dbProperties;
-  public final SystemDescription mainSystemDescription;
+    public final CanRegDAO canRegDAO;
+    public final Properties dbProperties;
+    public final SystemDescription mainSystemDescription;
 
-  /**
-   * Returns a CanRegDAO for the holding database of the API user
-   * @param apiUserName the userName of the api user
-   * @return CanRegDAO
-   */
-  public CanRegDAO getDaoForApiUser(String apiUserName) {
-    String registryCode = CanRegServerImpl.getRegistryCodeForApiHolding(
-        mainSystemDescription.getRegistryCode(), apiUserName, false);
-    return new CanRegDAO(registryCode, mainSystemDescription.getSystemDescriptionDocument(), dbProperties);
-  }
+    /**
+     * Returns a CanRegDAO for the holding database of the API user
+     * @param apiUserName the userName of the api user
+     * @return CanRegDAO
+     */
+    public CanRegDAO getDaoForApiUser(String apiUserName) {
+        // Additional variables for holding db
+        SystemDescription systemDescriptionForHoldingDB 
+                = HoldingDbCommon.buildSystemDescriptionForHoldingDB(mainSystemDescription);
+
+        String registryCode = CanRegServerImpl.getRegistryCodeForApiHolding(
+                mainSystemDescription.getRegistryCode(), apiUserName, false);
+        return new CanRegDAO(registryCode, systemDescriptionForHoldingDB.getSystemDescriptionDocument(), dbProperties);
+    }
 
 }
